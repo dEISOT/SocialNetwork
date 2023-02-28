2e7311c8a13982bfdb21a6ce29916a3de13ccc6b
Администратор <danielstudentbsu@gmail.com>
2023-02-28 18:21:15 +0300

    added message

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,6 +3,7 @@ import classes from "./Dialogs.module.css";
 import logo from "../../img/logo.jpg";
 import { NavLink } from "react-router-dom";
 import { DialogItem } from "./DialogsItem";
+import { Message } from "./Message";
 
 
 
@@ -17,11 +18,16 @@ export function Dialogs() {
                 <DialogItem name="Kostya" id="2"/>
                 <DialogItem name="Sergeevna" id="3"/>
                 <DialogItem name="Julia" id="4"/>
+                <DialogItem name="Sanya" id="5"/>
+                <DialogItem name="Egorik" id="6"/>
+                <DialogItem name="Dasha" id="7"/>
+
                         
              
             </div>    
             <div className={classes.messages}>
-                <h2>Hi sweetie</h2>
+                <Message />
+                
             </div>
         </div>
     );
